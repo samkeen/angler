a8c4fe03b8fb76115c51d571ce123ee76b8568b9
samkeen <sam@shopigniter.com>
2013-02-16 22:44:36 -0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Angler::Application.config.secret_token = '320c311183416268c9c33d3168da9d7863edb984cdacce5a1c5bb27b6c97d1d4db678bfec023130850395008d6f1043597405d2230e5c4a2125967425a08426d'
